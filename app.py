3aa6d77d6003740a71854a87407b722013592911
CarlosOrtiz94 <51864121+CarlosOrtiz94@users.noreply.github.com>
2022-10-20 19:29:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+# importar librerías
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn 
+import pickle
 
+#importar archivos
+
+data = pd.read_csv('')
+modelo = pickle.load(open('modelo.pickle', 'rb'))
+
+st.title('¿Habrías sobrevivido al Titanic?')
+
+
+
+# exploración
+
+st.header('Exploración inicial')
+
+#visualización
+
+#ingreso de datos
+
+#predicción
